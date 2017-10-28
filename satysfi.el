723c0fff9f3b6fa64d1a7f99f9c4652c3454ec85
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2017-10-28 20:07:20 +0900

    initial commit

---
+++ satysfi.el
@@ -0,0 +1,79 @@
+;;; satysfi.el --- SATySFi                      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  Takashi SUWA
+
+;; Author: Takashi SUWA
+;; Keywords:
+
+(provide 'satysfi)
+
+(defface satysfi-row-command-face
+  '((t (:foreground "#8888ff" :background "dark")))
+  "SATySFi row command")
+
+(defface satysfi-column-command-face
+  '((t (:foreground "#ff8888" :background "dark")))
+  "SATySFi column command")
+
+(defface satysfi-var-in-string-face
+  '((t (:foreground "#44ff88" :background "dark")))
+  "SATySFi variable in string")
+
+(defface satysfi-escaped-character
+  '((t (:foreground "#cc88ff" :background "dark")))
+  "SATySFi escaped character")
+
+(defface satysfi-literal-area
+  '((t (:foreground "#ffff44" :background "dark")))
+  "SATySFi literal area")
+
+(define-generic-mode satysfi-mode
+  '(?%)
+
+  '("let" "let-mutable" "let-row" "let-col" "in" "and"
+    "match" "with" "when" "as" "if" "then" "else"
+    "type" "constraint" "val" "direct" "of"
+    "module" "struct" "sig" "end"
+    "before" "while" "do"
+    "controls" "cycle")
+
+  '(("\\(\\\\\\(?:\\\\\\\\\\)*[a-zA-Z0-9\\-]+\\)\\>"
+     (1 'satysfi-row-command-face t))
+    ("\\(\\+[a-zA-Z0-9\\-]+\\)\\>"
+     (1 'satysfi-column-command-face t))
+    ("\\(@[a-z][0-9A-Za-z\\-]*\\)\\>"
+     (1 'satysfi-var-in-string-face t))
+    ("\\(\\\\\\(?:@\\|`\\|\\*\\| \\|%\\||\\|;\\|{\\|}\\|\\\\\\)\\)"
+     (1 'satysfi-escaped-character t))
+    ("\\(`\\(?:[^`]\\|\\n\\)+`\\)"
+     (1 'satysfi-literal-area t)))
+
+  nil
+  '())
+
+(defun satysfi-mode/insert-pair-scheme (open-string close-string)
+  (cond ((use-region-p)
+         (let ((rb (region-beginning)))
+           (let ((re (region-end)))
+             (progn
+               (goto-char rb)
+               (insert open-string)
+               (goto-char (1+ re))
+               (insert open-string)
+               (forward-char -1)))))
+        (t
+         (progn
+           (insert (format "%s%s" open-string close-string))
+           (forward-char -1)))))
+
+(defun satysfi-mode/insert-paren-pair ()
+  (interactive)
+  (satysfi-mode/insert-pair-scheme "(" ")"))
+
+(defun satysfi-mode/insert-brace-pair ()
+  (interactive)
+  (satysfi-mode/insert-pair-scheme "{" "}"))
+
+(defvar satysfi-mode-map (make-sparse-keymap))
+(define-key satysfi-mode-map (kbd "(") 'satysfi-mode/insert-paren-pair)
+(define-key satysfi-mode-map (kbd "{") 'satysfi-mode/insert-brace-pair)
